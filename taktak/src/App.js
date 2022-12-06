81521fd5df8050be72e5e514f956d477bf5e6900
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-12-05 21:10:13 -0500

    initial file structure

---
+++ taktak/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+
+import { Article, Brand, Cta, Feature, Navbar } from "./components";
+
+const App = () => {
+  return <div>GPT-3</div>;
+};
+
+export default App;
